e446d4c6ff7cc0f623d99d8d35b9c23ceb48612f
Snow3all <Netithorn_rly@hotmail.com>
2020-02-03 08:29:05 +0700

    CloudChat Ver0.1

---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="text-center">
+    <v-app id="inspire">
+      <v-content>
+        <v-container class="fill-height" fluid>
+          <v-row align="center" justify="center">
+            <v-col cols="12" sm="8" md="4">
+              <v-card class="elevation-12">
+                <v-toolbar color="primary" dark flat>
+                  <v-toolbar-title>CloudChat</v-toolbar-title>
+                  <v-spacer />
+                </v-toolbar>
+                <v-card-text>
+                  <v-form>
+                    <v-text-field
+                      label="Email or username"
+                      name="login"
+                      type="text"
+                    />
+                    <v-text-field
+                      id="password"
+                      label="Password"
+                      name="password"
+                      type="password"
+                    />
+                  </v-form>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer-center />
+                  <v-btn color="primary">Login</v-btn>
+                </v-card-actions>
+                  <v-card-actions>
+                      <v-spacer-center />
+                      <router-link to="Register"><v-btn color="primary">Register</v-btn></router-link>
+                  </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style scoped></style>
